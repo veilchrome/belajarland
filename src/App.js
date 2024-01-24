2606a521a0b853eaee65c3eb0327a8833bb48b65
veilchrome <keeita768900@gmail.com>
2024-01-24 23:00:40 +0700

    Pengerjaan Simple Project React 70%

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+import Home from "./pages";
+import About from "./pages/about";
+import Annual from "./pages/annual";
+import Blogs from "./pages/blogs";
+import Event from "./pages/events";
---
+++ src/pages/about.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <h1>Wikipedia Selamet Saputra adalah</h1>
+    </div>
+  );
+};
+
+export default About;
